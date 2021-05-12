7b97450ea708c3a199abdd00ddecd537ca16e647
LTHxxx <lthaler@free.fr>
2021-05-12 14:35:00 +0200

    Add files via upload

---
+++ HARD_SCARD.cmd
@@ -0,0 +1,22 @@
+powershell "Get-WinEvent @{logname='D‚veloppements Communs';ProviderName='HARD_SCARD';starttime=[datetime]::today.AddDays(-1) } |select TimeCreated,Message"
+pause
+
+
+rem au lieu|taper
+rem  de     |Alt+
+rem 
+rem  à 0133
+rem  â 0131
+rem  ä 0132
+rem  ç 0135
+rem  è 0138
+rem  é 0130
+rem  ê 0136
+rem  ë 0137
+rem  î 0140
+rem  ï 0139
+rem  ô 0147
+rem  ö 0148
+rem  ù 0151
+rem  û 0150
+rem  ü 0129
